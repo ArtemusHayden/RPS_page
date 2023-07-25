d234389084b66d779b694820e5dab484240f607a
Artemus Hayden <grayson2005hayden@gmail.com>
2023-07-25 11:27:10 -0500

    Add basic UI

--- index.js
+++ index.js
@@ -1,39 +1,6 @@
 COMP_SCORE = 0
 USER_SCORE = 0
 
-function fiveGames() {
-    for (let i = 0; i < 5; i++) {
-        let computerChoice = getComputerChoice();
-        let userChoice = getUserChoice();
-        computeWinner(computerChoice,userChoice);
-      }
-
-      if (USER_SCORE > COMP_SCORE) {
-        alert(`The game is over, the User won! User score was ${USER_SCORE} and Computer score was ${COMP_SCORE}.`)
-      } else if (COMP_SCORE > USER_SCORE){
-        alert(`The game is over, the Computer won! User score was ${USER_SCORE} and Computer score was ${COMP_SCORE}.`)
-      } else {
-        alert(`The game is over, it was a tie. User score was ${USER_SCORE} and Computer score was ${COMP_SCORE}.`)
-      }
-}
-
-function computeWinner(computerChoice,userChoice) {
-    console.log(userChoice);
-    console.log(computerChoice);
-    if ((userChoice === "paper" && computerChoice === "rock")
-        || (userChoice === "scissors" && computerChoice === "paper") 
-        || (userChoice === "rock" && computerChoice === "scissors")){
-        alert("You won!")
-        USER_SCORE++
-
-    } else if (userChoice === computerChoice) {
-        alert("It was a tie.")
-    } else {
-        alert("You lost!")
-        COMP_SCORE++
-    }
-}
-
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
 
@@ -46,14 +13,75 @@ function getComputerChoice() {
     }
 }
 
-function getUserChoice() {
-    let flag = "True";
-    while (flag === "True"){
-        let userChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-        if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
-            return userChoice;
+const clickRock = document.getElementById('rock');
+clickRock.addEventListener('click', () => {
+    playRound("rock");
+  });
+
+const clickPaper = document.getElementById('paper');
+clickPaper.addEventListener('click', () => {
+    playRound("paper");
+  });
+
+const clickScissors = document.getElementById('scissors');
+clickScissors.addEventListener('click', () => {
+    playRound("scissors");
+});
+
+function playRound(userChoice) {
+    const computerChoice = getComputerChoice();
+    computeWinner(computerChoice,userChoice);
+    checkGameCondition();
+    updateScore();
+}
+
+function checkGameCondition() {
+    if (USER_SCORE >= 5 || COMP_SCORE >= 5) {
+        if (USER_SCORE > COMP_SCORE){
+            results.textContent = (`The game is over, the User won! User score was ${USER_SCORE} and Computer score was ${COMP_SCORE}.`)
+        }  else if (COMP_SCORE > USER_SCORE){
+            results.textContent = (`The game is over, the Computer won! User score was ${USER_SCORE} and Computer score was ${COMP_SCORE}.`)
+        } else {
+            results.textContent = (`The game is over, it was a tie. User score was ${USER_SCORE} and Computer score was ${COMP_SCORE}.`)
         }
+    COMP_SCORE = 0
+    USER_SCORE = 0
     }
 }
 
-fiveGames();
+function computeWinner(computerChoice,userChoice) {
+    console.log(`user: ${userChoice}`)
+    console.log(`comp: ${computerChoice}`)
+    if ((userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" && computerChoice === "paper") || (userChoice === "rock" && computerChoice === "scissors")){
+        results.textContent = ("The user won this round!");
+        USER_SCORE++
+    } else if (userChoice === computerChoice) {
+        results.textContent = ("The user and computer tied!")
+    } else {
+        results.textContent = ("The computer won this round!");
+        COMP_SCORE++
+    }
+}
+
+function updateScore(){
+    playerScore.textContent = (`The player score is ${USER_SCORE}`);
+    compScore.textContent = (`The computer score is ${COMP_SCORE}`);
+}
+
+const resultContainer = document.querySelector('.result-container');
+
+const playerScore = document.createElement('p');
+playerScore.classList.add('playerScore');
+playerScore.textContent = (`The player score is ${USER_SCORE}`);
+resultContainer.appendChild(playerScore);
+
+const compScore = document.createElement('p');
+compScore.classList.add('compScore');
+compScore.textContent = (`The computer score is ${COMP_SCORE}`);
+resultContainer.appendChild(compScore);
+
+
+const results = document.createElement('p');
+results.classList.add('results');
+results.textContent = ("Goodluck!");
+resultContainer.appendChild(results);
